fc0202df912ba07fb1dae822c946684bbb759244
Seu Nome Rafael <rafaellira@poli.ufrj.br>
2021-02-28 16:40:39 -0300

    Projeto 3 telas fundo preto

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new FormCadastroDeAlunos());
         }
     }
 }
